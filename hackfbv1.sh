206d69f5a3734cb899a6f4d3dfefaf05e84a1364
Ripal2204 <52343663+Ripal2204@users.noreply.github.com>
2019-07-01 07:01:00 +0700

    Masih anget nih guys:)

---
+++ hackfbv1.sh
@@ -0,0 +1,191 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Jangan Recode Ya gayn
+echo
+echo
+echo
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo $i" |"$cy"     INDONESIA DARK TERMUX ASSOCIATE"$i"         |"
+echo $i" |"$me"─────────────────────────────────────────────"$i"|"
+echo $i" |"$pu" AUTHOR :"$ku" MUHAMAD RIPAL  [FR13ND8]"$i"           |"
+echo $i" |"$pu" FACEBOOK :"$ku" Ripal Nyaho Meren"$i"                |"
+echo $i" |"$pu" THANKS TO :"$cy"~ [MR.P3RC1L] [MR.S∆MAx] [MR.B]"$i"  |"
+echo $i" |"$cy"            ~ [cyber] [%MR.S%] [AldykaPrygh]"$i" |"
+echo $i" |"$pu" Contack WhatsApp :"$ku" 085886755753"$i"             |"
+echo $i" |"$pu" Contack Gmail :"$ku" muhamadripal752@gmail.com"$i"   |"
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────"$i"["$bi"•"$i"]"
+echo
+echo $red"=========================================================="
+echo $red"|NO|                   TOOLS                             |"
+echo $b"=========================================================="
+echo $me"|1.|HACK FACEBOOK MBF                                    |"
+echo $me"|2.|HACK FACEBOOK BRUTEFORCENEW                          |"
+echo $pu"|3.|HACK FACEBOOK RUSMANA                                |"
+echo $pu"|4.|HACK FACEBOOK DARK FB                                |"
+echo $cy"|5.|HACK FACEBOOK                                        |"
+echo $cy"|6.|LITE SPAM                                            |"
+echo $b"|7.|DDOS ATTACK HAMMER                                   |"
+echo $b"|8.|DDOS ATTACK2                                         |"
+echo $i"|9.|MENAMBAHKAN TOMBOL KANAN & KIRI TERMUX               |"
+echo $i"|10|INSTALL BAHAN DULU COEG                              |"
+echo $ku"|11|EXIT/KELUAR PROGRAM                                  |"
+echo $ku"=========================================================="
+echo
+echo
+echo $me"┌==="$bi"["$i"MUHAMAD RIPAL"$bi"]"$me"======"$bi"["$i""Pilih Nomornya""$bi"]"
+echo $me"¦"
+read -p"└──# " pil
+
+
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "Tunggu Bos"|lolcat
+sleep 1
+git clone https://github.com/kece1/mbf
+Cd mbf
+Python2 mbf.py
+fi
+
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "Tunggu Bos"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/BRUTEFORCEnew
+cd BRUTEFORCEnew
+sh new.sh
+fi
+
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "Tunggu Bos"|lolcat
+sleep 1
+git clone https://github.com/Rusmana-ID/rus
+cd rus
+sh v2.sh
+fi
+
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "Tunggu Bos"|lolcat
+sleep 1
+pkg install git
+git clone https://github.com/ZeDDParker/dark-fb
+cd drak-fb
+python2 dark.py
+fi
+
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "Tunggu Bos"|lolcat
+sleep 1
+git clone https://github.com/pashayogi/SETAN
+cd SETAN
+pip2 install mechanize
+pip install requests
+pip2 install requests
+python2 SETAN.py
+fi
+
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "Tunggu Bos"|lolcat
+sleep 1
+git clone https://github.com/4L13199/LITESPAM
+
+cd LITESPAM
+
+sh LITESPAM.sh
+fi
+
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "Tunggu Bos"|lolcat
+sleep 1
+git clone https://github.com/cyweb/hammer
+cd hammer
+python hammer.py
+fi
+
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "Tunggu Bos"|lolcat
+sleep 1
+apt update && apt upgrade
+apt install git toilet
+apt install python
+apt install python2
+git clone https://github.com/banghyuu/ddosWD
+cd ddosWD
+sh Ddos.sh
+fi
+
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "Tunggu Bos"|lolcat
+sleep 1
+pkg install python
+pkg install git
+git clone https://github.com/karjok/terkey
+cd terkey
+python terkey.py
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "Install Bahan"|lolcat
+sleep 1
+pkg update
+pkg upgrade
+pkg install python2
+pkg install mechanize
+pkg install requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install lolcat
+gem install lolcat
+pkg install libcurl
+sh hackfbv1.sh
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $cy"Terima Kasih Sudah Pakai Tool Saya"
+sleep 2
+echo $i"Bila Ada Kesalahan Kamu Bisa Nanya Kepada Saya"
+sleep 2
+echo $ku"WhatsApp :"$i" 085886755753"
+echo $ku"Facebook :"$i" Ripal Nyaho Meren"
+sleep 2
+echo $pur">> SALAM HACKER  <<"
+exit
+fi
